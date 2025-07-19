0376ba3322b641f49f7a4448660e87104b802078
Gorazd <fi.gorazd@gmail.com>
2025-07-19 18:24:53 +0200

    add multiple models at once

--- client-test/src/main/java/com/projectgoch/clienttest/controller/MyController.java
+++ client-test/src/main/java/com/projectgoch/clienttest/controller/MyController.java
@@ -1,7 +1,7 @@
 package com.projectgoch.clienttest.controller;
 
 import org.springframework.ai.chat.client.ChatClient;
-import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,10 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 class MyController {
 
-    private final ChatClient chatClient;
+    private final ChatClient openAiChatClient;
+    private final ChatClient anthropicChatClient;
 
-    public MyController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public MyController( @Qualifier("openAiChatClient") ChatClient openAiChatClient,
+                         @Qualifier("anthropicChatClient") ChatClient anthropicChatClient) {
+        this.openAiChatClient = openAiChatClient;
+        this.anthropicChatClient = anthropicChatClient;
     }
 
     @GetMapping("/")
@@ -24,10 +27,17 @@ class MyController {
 
     @PostMapping("/ai")
     public String generation(@RequestParam String userInput, Model model) {
-        String response = this.chatClient.prompt()
+        String response1 = this.openAiChatClient.prompt()
                 .user(userInput)
                 .call()
                 .content();
+
+        String response2 = this.anthropicChatClient.prompt()
+                .user(userInput)
+                .call()
+                .content();
+
+        String response = "openai:\n" + response1 + "\nanthropic: \n" + response2;
 
         model.addAttribute("userInput", userInput);
         model.addAttribute("aiResponse", response);
